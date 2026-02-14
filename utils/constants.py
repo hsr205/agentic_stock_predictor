ac57d3fed527cf95522c95b7993050a608fb7ae4
Henry Rothenberg <hsr205@aol.com>
2026-02-14 12:11:08 -0500

    Add constants.py module

---
+++ utils/constants.py
@@ -0,0 +1,5 @@
+class Constants:
+    LOGGER_COLOR_RESET: str = "\033[0m"
+    LOGGER_COLOR_WHITE: str = "\033[60m"
+    LOGGER_COLOR_ORANGE: str = "\033[33m"
+    LOGGER_COLOR_DARK_RED: str = "\033[31m"
