e7db7bb5858e588de902e53a3c5c0c39b14a99b4
Henry Rothenberg <hsr205@aol.com>
2026-02-14 12:34:31 -0500

    Add demo logic to intraday_market_data.py module

--- data/intraday_market_data.py
+++ data/intraday_market_data.py
@@ -1,22 +1,21 @@
+import pandas as pd
+import yfinance as yf
+
 from config.config import settings
-from alpha_vantage.timeseries import TimeSeries
 from logger.logger import AppLogger
+
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", None)
+
 
 class IntraDayMarketData:
 
     def __init__(self) -> None:
-        self.api_key:str = settings.alpha_vantage_api_key
-        self._logger = AppLogger.get_logger(self.__class__.__name__)
-
-
+        self._api_key: str = settings.api_key
+        self.logger = AppLogger.get_logger(self.__class__.__name__)
 
     def get_market_data(self) -> None:
-        time_series_data:TimeSeries = TimeSeries(key=self.api_key)
-        data, meta = time_series_data.get_intraday('AAPL', interval='1min', outputsize='full')
+        dataframe: pd.DataFrame = yf.download("AAPL", start="2026-02-01", end="2026-02-08", interval="1m")
 
-        print(f"len(data) = {len(data)}")
-        print(f"meta = {meta}")
-
-
-
-
+        self.logger.info(f"data.columns = {dataframe.columns}")
+        print(dataframe.head())
