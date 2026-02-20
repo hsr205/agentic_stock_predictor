42385365f34f538b72c0bffef5db4d0140ff423c
Henry Rothenberg <hsr205@aol.com>
2026-02-20 17:19:07 -0500

    Alter demo logic in main.py module

--- main/main.py
+++ main/main.py
@@ -1,5 +1,6 @@
 from logging import Logger
 
+from data.alpaca_historic_data import AlpacaHistoricData
 from trading_account.alpaca_trading_account import AlpacaTradingAccount
 from logger.logger import AppLogger
 from alpaca.trading.enums import OrderSide
@@ -8,10 +9,13 @@ def main() -> int:
     logger: Logger = AppLogger().get_logger(__name__)
 
     try:
-        alpaca_trading_account: AlpacaTradingAccount = AlpacaTradingAccount()
-        # alpaca_trading_account.execute_action(ticker_str="AMZN",quantity=1,action_type=OrderSide.BUY)
-        alpaca_trading_account.get_portfolio_positions_dict()
-        # alpaca_trading_account.get_account_data_dict()
+        alpaca_historic_data:AlpacaHistoricData = AlpacaHistoricData()
+        alpaca_historic_data.export_historical_stock_data()
+
+        # alpaca_trading_account: AlpacaTradingAccount = AlpacaTradingAccount()
+        # # alpaca_trading_account.execute_action(ticker_str="AMZN",quantity=1,action_type=OrderSide.BUY)
+        # # alpaca_trading_account.get_portfolio_positions_dict()
+        # # alpaca_trading_account.get_account_data_dict()
 
 
     except Exception as e:
