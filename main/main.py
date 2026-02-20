88ebf3bd5f51bb5dd6a577d8e84b77595be7cd12
Henry Rothenberg <hsr205@aol.com>
2026-02-20 11:57:52 -0500

    Reformat main.py module

--- main/main.py
+++ main/main.py
@@ -1,14 +1,16 @@
+from logging import Logger
+
 from data.intraday_market_data import IntraDayMarketData
 from logger.logger import AppLogger
-from logging import Logger
 
 
 def main() -> int:
-    logger:Logger = AppLogger().get_logger(__name__)
+    logger: Logger = AppLogger().get_logger(__name__)
 
     try:
         intraday_market_data: IntraDayMarketData = IntraDayMarketData()
         intraday_market_data.get_market_data()
+
     except Exception as e:
         logger.info(f"Exception Thrown: {e}")
 
