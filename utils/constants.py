72f4af601bfa4e0a4b40a69718a1585abef4d191
Henry Rothenberg <hsr205@aol.com>
2026-02-20 15:12:40 -0500

    Add field to constants.py module

--- utils/constants.py
+++ utils/constants.py
@@ -4,4 +4,9 @@ class Constants:
     LOGGER_COLOR_ORANGE: str = "\033[33m"
     LOGGER_COLOR_DARK_RED: str = "\033[31m"
 
+    ALPACA_ACCOUNT_URL: str = "https://paper-api.alpaca.markets/v2/account"
+
     TICKER_SYMBOL_LIST: list[str] = ["TESLA", "AAPL", "META", "AMZN", "MSFT", "NVDA", "GOOGL"]
+
+
+
