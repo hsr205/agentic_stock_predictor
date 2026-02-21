3aa75e36fd6fd140ab723d1c80957324ecdf53ad
Henry Rothenberg <hsr205@aol.com>
2026-02-21 13:58:19 -0500

    Add field to constants.py module

--- utils/constants.py
+++ utils/constants.py
@@ -1,9 +1,14 @@
+from alpaca.trading.enums import OrderSide
+
 class Constants:
     LOGGER_COLOR_RESET: str = "\033[0m"
     LOGGER_COLOR_WHITE: str = "\033[60m"
     LOGGER_COLOR_ORANGE: str = "\033[33m"
     LOGGER_COLOR_DARK_RED: str = "\033[31m"
 
+    ACTIONS_LIST:list[OrderSide] = [OrderSide.BUY, OrderSide.SELL]
+
     ALPACA_ACCOUNT_URL: str = "https://paper-api.alpaca.markets/v2/account"
 
     TICKER_SYMBOL_LIST: list[str] = ["TSLA", "AAPL", "META", "AMZN", "MSFT", "NVDA", "GOOGL"]
+
