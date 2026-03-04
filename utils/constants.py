14ed0096ea9f2dc2ab8243a4c8b88bc87d4937b5
Henry Rothenberg <hsr205@aol.com>
2026-03-04 14:33:34 -0500

    Add field to constants.py module

--- utils/constants.py
+++ utils/constants.py
@@ -12,5 +12,15 @@ class Constants:
     ORDER_SIDE_ACTIONS_LIST: list[OrderSide] = [OrderSide.BUY, OrderSide.SELL]
     ACTIONS_LIST: list[OrderSide | str] = [OrderSide.BUY, OrderSide.SELL, "HOLD"]
 
-    TICKER_SYMBOL_LIST: list[str] = ["AAPL", "AMZN", "GOOGL", "META", "NVDA", "MSFT", "TSLA"]
+    TICKER_SYMBOL_TO_ID: dict[str, int] = {
+        "AAPL": 1_001,
+        "AMZN": 1_002,
+        "GOOGL": 1_003,
+        "META": 1_004,
+        "MSFT": 1_005,
+        "NVDA": 1_006,
+        "TSLA": 1_007
+    }
+
+    TICKER_SYMBOL_LIST: list[str] = ["AAPL", "AMZN", "GOOGL", "META", "MSFT", "NVDA", "TSLA"]
     CSV_OUTPUT_COLUMNS_LIST: list[str] = ["Timestep", "Timestamp", "Portfolio Equity", "Portfolio Cash Available"] + TICKER_SYMBOL_LIST
